7be678c1aa3c6369b2ddcf4f5122c6cbf245a51a
Nikhil <nikiljana123@gmail.com>
2024-02-08 15:03:32 +0530

    Valid Last Name

--- Regex/src/com/bridgelabz/regex/ValidateUserDetails.java
+++ Regex/src/com/bridgelabz/regex/ValidateUserDetails.java
@@ -6,8 +6,14 @@ import java.util.regex.Pattern;
 public class ValidateUserDetails {
 
     public boolean isValidFirstName(String firstName){
-        Pattern pattern = Pattern.compile(firstName);
-        Matcher matcher = pattern.matcher("^[A-Z][a-zA-Z]{2,}$");
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}+$");
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    public boolean isValidLastName(String lastName){
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}+$");
+        Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
     }
 }
--- Regex/src/com/bridgelabz/regex/userRegistration.java
+++ Regex/src/com/bridgelabz/regex/userRegistration.java
@@ -8,11 +8,23 @@ public class userRegistration {
         String firstName;
         boolean x;
         ValidateUserDetails validate = new ValidateUserDetails();
+
+        //Validate First Name
         do {
             System.out.println("Enter your first name (min 3 characters required, starting with a capital letter): ");
             firstName = scanner.nextLine();
             x = validate.isValidFirstName(firstName);
-        } while (x);
+        } while (!x);
         System.out.println("Valid first name: " + firstName);
+
+        
+        //Validate Last Name
+        String lastName;
+        do {
+            System.out.println("Enter your last name (min 3 characters required, starting with a capital letter): ");
+            lastName = scanner.nextLine();
+            x = validate.isValidLastName(lastName);
+        } while (!x);
+        System.out.println("Valid last name: " + lastName);
     }
 }
